696d28a2616d3716d1864246883e52b934e9e39f
Jstambaugh00 <Jstambaugh00@gmail.com>
2022-04-08 15:16:58 -0500

    Recreating initial code using pytorch. Switched for easier manipulation down the road.

--- scratch.py
+++ scratch.py
@@ -1,6 +1,17 @@
-import utils
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
-a = ['A', 'G', 'T', 'C']
-b = [1, 2, 3, 4]
+model = keras.Sequential()
+# Add an Embedding layer expecting input vocab of size 1000, and
+# output embedding dimension of size 64.
+model.add(layers.Embedding(input_dim=1000, output_dim=64))
 
-print(utils.str_to_num(a))
+# Add a LSTM layer with 128 internal units.
+model.add(layers.LSTM(128))
+
+# Add a Dense layer with 10 units.
+model.add(layers.Dense(10))
+
+model.summary()
--- train.py
+++ train.py
@@ -1,13 +1,17 @@
-import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+import pandas as pd
 
 def main():
     # Load data
-    X_train = np.loadtxt(open("/Users/jacobstambaugh/Documents/RNN-VirSeeker/data/rnn_train.csv", "rb"), delimiter=",", skiprows=0)
-    y_train = np.loadtxt(open("/Users/jacobstambaugh/Documents/RNN-VirSeeker/data/label_train.csv", "rb"), delimiter=",", skiprows=0)
+    x_train = pd.read_csv("/Users/jacobstambaugh/Documents/RNN-VirSeeker/data/rnn_train.csv")
+    y_train = pd.read_csv("/Users/jacobstambaugh/Documents/RNN-VirSeeker/data/label_train.csv", usecols=[0])
+
+    # TODO: Implement training + testing split
 
     # model parameters
     learning_rate = 0.00001
@@ -15,9 +19,12 @@ def main():
     batch_size = 256
     display_step = 10
     seq_max_len = 500
-    n_hidden = 256
+    n_hidden = 256  # number of units in RNN cell
     n_classes = 2
 
+    # convert to tensors
+    x_train_tensors = Variable(torch.Tensor(x_train))
+    y_train_tensors = Variable(torch.Tensor(y_train))
 
 
 main()
